46d42314c978719d50921fe9a849c1bb7275b86d
Felipe Santos <felipegssantos@gmail.com>
2019-10-18 18:09:05 -0300

    Add unit tests for Rumba and fix Rumba's seed method

--- scala/src/main/scala/pieces/Rumba.scala
+++ scala/src/main/scala/pieces/Rumba.scala
@@ -3,7 +3,7 @@ package pieces
 class Rumba(var rumbaPopulation: Int = 0) extends Hole(rumbaPopulation) {
 
     override def seed(numSeeds: Int): Int = {
-        rumbaPopulation += 1
+        population += 1
         numSeeds - 1
     }
     
--- scala/src/test/scala/pieces/RumbaTest.scala
+++ scala/src/test/scala/pieces/RumbaTest.scala
@@ -1,5 +1,30 @@
 package pieces
 
-class RumbaTest {
+import org.scalatest.FlatSpec
+
+class RumbaTest extends FlatSpec {
+
+  "A Rumba(2)" should "return the integer 2 and end up with zero population when harvested" in {
+    val rumba = new Rumba(2)
+    val seeds = rumba.harvest
+    assert(rumba.getPopulation == 0)
+    assertResult(2)(seeds)
+  }
+
+  it should "return false when isEmpty is invoked" in {
+    val rumba = new Rumba(2)
+    assert(!rumba.isEmpty)
+  }
+
+  "A Rumba(0)" should "have population equal to 1 when 1 seed is put into it" in {
+    val rumba = new Rumba(0)
+    rumba.seed(1)
+    assertResult(1)(rumba.getPopulation)
+  }
+
+  it should "return true when isEmpty is invoked" in {
+    val rumba = new Rumba(0)
+    assert(rumba.isEmpty)
+  }
 
 }
