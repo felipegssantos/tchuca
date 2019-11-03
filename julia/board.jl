692cb16aacfbc3455a9c3dcc2605f726c2cf7bbd
Felipe Santos <felipegssantos@gmail.com>
2019-11-03 17:37:12 -0300

    Simplify board type

--- julia/board.jl
+++ julia/board.jl
@@ -1,28 +1,27 @@
 include("./hole.jl")
 
-struct Board
-    holes::Array{BaseHole}
+using Base.Iterators
 
-    function Board(numHoles::Int)
-        holes = [(i > 1 ? Hole(2) : Rumba(0)) for i in 1:numHoles]
-        new(holes)
-    end
-end
+buildboard(numHoles::Int) = [(i > 1 ? Hole(2) : Rumba(0)) for i in 1:numHoles]
 
-function Base.show(io::IO, board::Board)
-    output = [" $(hole.population) " for hole in board.holes]
+function Base.show(io::IO, board::Array{BaseHole})
+    output = [" $(hole.population) " for hole in board]
     output = join(output, "|")
     print(output)
 end
 
-function harvest&seedall(board::Board, holeIndex::Int)
-    seeds = harvest(board.holes(holeIndex))
-    seedall(board, nextHole(board, holeIndex))
+function harvest&seedall(board::Array{BaseHole}, holeIndex::Int)
+    seeds = harvest(board[holeIndex])
+    seedall(board, nextHole(board, holeIndex) seeds)
 end
 
-function seedall(board::Board, holeIndex::Int)
+function seedall(board::Array{BaseHole}, holeIndex::Int, numSeeds::Int)
     # TODO
-    # Ideas: use an cyclical iterator; use an "until"-like macro;
+    # Ideas: use an cyclical iterator; use an "until"-like macro; use recursion
 end
 
-nextHole(board::Board, holeIndex::Int) = (holeIndex + 1) % len(board.holes)
+function seed(board::Array{BaseHole}, holeIndex::Int)
+    [i == holeIndex ? seed(hole) : hole for hole, i in enumerate(board)]
+end
+
+nextHole(board::Array{BaseHole}, holeIndex::Int) = (holeIndex + 1) % size(board)[1]
--- julia/game.jl
+++ julia/game.jl
@@ -1,8 +1,8 @@
-include("./game.jl")
+include("./board.jl")
 
 # TODO: print board highlighting current hole
 # TODO: implement the rest of the game
-board = Board(5)
+board = buildboard(5)
 print("Escolha o primeiro buraco para colher: ")
 hole_index = parse(Int, readline(stdin))
 println(hole_index)
